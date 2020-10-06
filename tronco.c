5852f4a1dfe05837a177d311331ffa78db3d8825
Anthony Chitay <antonchitay@icloud.com>
2020-10-06 17:20:41 -0600

    tronco

---
+++ tronco.c
@@ -0,0 +1,29 @@
+#include <linux/module.h>              /* Todos los modulos lo necesitan */
+#include <linux/kernel.h>               /* Ofrece la macro KERN_INFO */
+#include <linux/init.h>                   /* Ofrece las macros de inicio y fin */
+
+static int __init troncostart(void)
+{
+        printk(KERNINFO "Que pasa tronco!\n");
+        /*
+         * Si no se devuelve 0 significa que initmodule ha fallado y no ha podido cargarse.
+         */
+        return 0;
+}
+static void __exit troncocleanup(void)
+{
+        printk(KERNINFO "Me voy dando el piro!\n");
+}
+
+/*
+ * Indicamos cuales son las funciones de inicio y fin
+ */
+moduleinit(troncostart);
+moduleexit(troncocleanup);
+/*
+ * Documentacion del modulo
+ */
+MODULELICENSE("GPL");
+MODULEAUTHOR("Genbeta Dev - https://www.genbetadev.com");
+MODULEDESCRIPTION("Este modulo no vale para absolutamente nada");
+MODULESUPPORTEDDEVICE("eltiodelavara");
