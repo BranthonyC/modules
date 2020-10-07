a4841ea88f5836f9f8faf727de0371dc69282fc5
Anthony Chitay <antonchitay@icloud.com>
2020-10-06 23:28:38 -0600

    fixin issues

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -112,8 +112,6 @@ static struct file_operations my_file_ops = {
  */
 int init_module(void)
 {
-	struct proc_dir_entry *entry;
-
 	proc_create(PROC_NAME, 0, NULL, &my_file_ops);
 	// if (entry) {
 	// 	entry->proc_fops = &my_file_ops;
