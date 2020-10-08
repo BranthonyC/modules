a171d768b0b6bbb5d7218a018d62b73050ce5b82
Anthony Chitay <antonchitay@icloud.com>
2020-10-07 19:00:08 -0600

    cpu

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -44,7 +44,7 @@ static int escribir_archivo(struct seq_file * archivo,void *v){
      seq_printf(archivo, "                                                            \n");
 
      for_each_process( task ){            /*    for_each_process() MACRO for iterating through each task in the os located in linux\sched\signal.h    */
-        seq_printf(archivo, "{\"PADRE\": %d , \"PID\": %d , \"NOMBRE\": \"%s\" , \"STADO\": %ld, \"UTIME\": %lli, \"STIME\": %lli, \"uptime\":%li  }\n",task->pid,task->pid, task->comm, task->state, task->utime, task->stime, uptime);/*    log parent id/executable name/state    */
+        seq_printf(archivo, "{\"PADRE\": %d , \"PID\": %d , \"NOMBRE\": \"%s\" , \"STADO\": %ld, \"UTIME\": %lli, \"STIME\": %lli, \"uptime\":%li  }\n",task->pid,task->pid, task->comm, task->state, task->utime, task->stime, uptime.tv_sec);/*    log parent id/executable name/state    */
         
     }    
      seq_printf(archivo, "*******************************************************************************************\n");
