1de31a39b7661e478145729165b3cf2424b2dd3c
Anthony Chitay <antonchitay@icloud.com>
2020-10-08 23:02:31 -0600

    avance

--- cpu_module/cpu.c
+++ cpu_module/cpu.c
@@ -155,11 +155,11 @@ static int show_stat(struct seq_file *p, void *v)
         t_total = user+nice+system+idle+iowait+irq+softirq+steal;
         t_idle = idle+iowait;
         t_usage = t_total - t_idle;
+        u64 percentage = 100;
         seq_printf(p, "t_total: %lld \n", t_total);
         seq_printf(p, "t_idle: %lld \n", t_idle);
         seq_printf(p, "t_usage: %lld \n",t_usage);
-        seq_printf(p, "cpu: : %lld", div64_u64(t_usage, t_total));
-        seq_printf(p, "cpu%d", i);
+        seq_printf(p, "cpu: : %lld", percentage*div64_u64(t_usage, t_total));
 		seq_putc(p, '\n');
 	}
 	seq_put_decimal_ull(p, "intr ", (unsigned long long)sum);
