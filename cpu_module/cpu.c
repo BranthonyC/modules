aa7f03881285df8e5c053faf8f20ec03866894bf
Anthony Chitay <antonchitay@icloud.com>
2020-10-08 22:18:42 -0600

    avance

--- cpu_module/cpu.c
+++ cpu_module/cpu.c
@@ -110,15 +110,15 @@ static void show_irq_gap(struct seq_file *p, unsigned int gap)
 
 static void show_all_irqs(struct seq_file *p)
 {
-// 	unsigned int i, next = 0;
+	unsigned int i, next = 0;
 
-// 	for_each_active_irq(i) {
-// 		show_irq_gap(p, i - next);
-// 		seq_put_decimal_ull(p, " ", kstat_irqs_usr(i));
-// 		next = i + 1;
-// 	}
-// 	show_irq_gap(p, nr_irqs - next);
-// }
+	for_each_active_irq(i) {
+		show_irq_gap(p, i - next);
+		seq_put_decimal_ull(p, " ", kstat_irqs_usr(i));
+		next = i + 1;
+	}
+	show_irq_gap(p, nr_irqs - next);
+}
 
 static int show_stat(struct seq_file *p, void *v)
 {
